e6215f02f6e510dabbd7d7e2d0099c88f76415c0
Harsha-Varthan <harshavarthan56@gmail.com>
2024-11-27 16:37:05 -0600

    Update

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
-let
-  my-python-packages = ps: with ps; [
-    pycrypto
-    flask
-    requests
-    # other python packages
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.grpc
+    pkgs.protobuf
+    pkgs.cmake
+    pkgs.gcc
   ];
-  my-python = pkgs.python39.withPackages my-python-packages;
-in my-python.env
+}
